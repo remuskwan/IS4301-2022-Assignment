a74ad383f19d56e6913cd05fe3ab6e56810f31f6
Remus <remuskwan23@gmail.com>
2022-10-22 20:47:16 +0800

    feat: updated favicon and logo

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,5 +1,6 @@
-import { Link } from "react-router-dom";
-import utownGreen from '../../assets/utown-green.jpeg'
+import { Link } from 'react-router-dom';
+import utownGreen from '../../assets/utown-green.jpeg'; 
+// import NusCoat from '../vectors/NusCoat';
 
 const Hero = () => {
   return (
@@ -8,20 +9,21 @@ const Hero = () => {
       <div className='mx-auto max-w-7xl sm:px-6 lg:px-8'>
         <div className='relative shadow-xl sm:overflow-hidden sm:rounded-2xl'>
           <div className='absolute inset-0'>
-            <img
-              className='h-full w-full object-cover'
-              src={utownGreen}
-              alt='UTown Green'
-            />
+            <img className='h-full w-full object-cover' src={utownGreen} alt='UTown Green' />
             <div className='absolute inset-0 bg-indigo-700 mix-blend-multiply' />
           </div>
           <div className='relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8'>
+            {/* <div className='relative flex justify-center items-center'>
+              <div className='absolute inset-x-0 block h-26 w-auto'>
+                <NusCoat />
+              </div>
+            </div> */}
             <h1 className='text-center text-4xl font-bold tracking-tight sm:text-5xl lg:text-6xl'>
               <span className='block text-white'>National University</span>
               <span className='block text-indigo-200'>of Singapore</span>
             </h1>
             <p className='mx-auto mt-6 max-w-lg text-center text-xl text-indigo-200 sm:max-w-3xl'>
-            NUS is a leading research university in Asia.
+              NUS is a leading research university in Asia.
             </p>
             <div className='mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center'>
               <div className='space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0'>
